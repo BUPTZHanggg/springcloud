35ad7e188541b5cba7e812aa91887eb09efc1d50
Hang <ZhaoHang@bupt.edu.cn>
2019-05-25 11:41:05 +0800

    a

--- microservicecloud-consumer-dept-80/src/main/java/com/megvii/springcloud/cfgbeans/ConfigBean.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/megvii/springcloud/cfgbeans/ConfigBean.java
@@ -14,7 +14,6 @@ public class ConfigBean{//boot -->spring   applicationContext.xml --- @Configura
 	@Bean
 	@LoadBalanced //Spring Cloud Ribbon是基于Netflix Ribbon实现的一套客户端       负载均衡的工具。
 	public RestTemplate getRestTemplate(){
-		System.out.println("a");
 		return new RestTemplate();
 	}
 
